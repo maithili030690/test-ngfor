2be3366f7de0c5aa939e45ee46b0fa5f641110c1
maithili030690 <maithili.shimpankar@gmail.com>
2024-10-05 17:17:44 +0530

    test-ngfor-postcard-moviecard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Iposts } from './shared/models/posts';
+import { postArr } from './shared/consts/posts';
+import { Imovie } from './shared/models/movie';
+import { movieArray } from './shared/consts/movie';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-ngfor';
+  postData !:Array<Iposts>
+  movieData! :Array<Imovie>
+
+  ngOnInit(): void {
+    this.postData = postArr
+    this.movieData=movieArray
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostCardComponent } from './shared/component/post-card/post-card.component';
+import { MovieCardComponent } from './shared/component/movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCardComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule
